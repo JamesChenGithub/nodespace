844ba6924799c42ce9ea72f1184e2b5db0ad7d5c
alexichen <403725592@qq.com>
2020-03-04 00:15:38 +0800

    更新index.html

--- wxwork/server.js
+++ wxwork/server.js
@@ -3,7 +3,13 @@ const app = express();
 // const bodyparser = require('body-parser');
 // const path = require('path');
 
-// app.use('/', express.static('./static/'));
+app.use('/', express.static('./static/'));
+
+// 定位到登录页面
+app.get('/', function (req, res) {
+    console.log('调用 / 页面');
+    res.sendFile(__dirname + '/index.html');
+})
 
 // app.use(bodyparser.urlencoded({extended:false}));
 // app.use(bodyparser.json());
